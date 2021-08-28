b1e640ada4a17a87d279fc667736e41299c0a302
Gya7329 <gya7329@gmail.com>
2021-08-28 19:01:35 +0530

    Created Homepage & Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Homepage from './components/Homepage/Homepage'
 
-import { BrowserRouter } from 'react-router-dom';
+import LogOrsign from './components/Login-Signup/LogOrsign'
+import Signup from './components/Login-Signup/Signup'
+import Navbar from './components/Navbar/Navbar';
+
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <BrowserRouter>
-    
-    <div style={{ overflowX: "hidden" }}>
-    <Navbar />
+    <div className="App">
+      <Router>
+        <Switch>
+          {/* <Route path ="/" exact render={props=><Navbar{...props}/>}/> */}
+          <Route path="/" exact render={props => <Homepage {...props} />} />
+          <Route path="/login" render={props => <LogOrsign {...props} />} />
+          <Route path="/register" render={props => <Signup {...props} />} />
+      
+        </Switch>
+      </Router>
     </div>
-    </BrowserRouter>
-    
+
   );
 }
 
